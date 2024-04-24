c251cbc8f3c3449534a9996bc34a15bd6f4df557
Garrick Larson Potts <ricki.glp@gmail.com>
2024-04-24 14:03:35 +0000

    task 9 thorw an error

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
+
+  queue.push('Guardrail was processed');
+  return queue;
+}
