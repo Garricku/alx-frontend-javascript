5902e171863fd9861d10a5b058fa1e205e9b4382
Garrick Larson Potts <ricki.glp@gmail.com>
2024-04-24 13:08:16 +0000

    task 3 all promises importing with as

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,17 @@
+import * as p from './utils';
+
+function handleProfileSignup() {
+  const profilePromise = p.uploadPhoto();
+  const userPromise = p.createUser();
+
+  Promise.all([profilePromise, userPromise])
+    .then(([profile, user]) => {
+      console.log(profile.body, user.firstName, user.lastName);
+    })
+    .catch((error) => {
+      console.error('Signup system offline');
+      return error;
+    });
+}
+
+export default handleProfileSignup;
