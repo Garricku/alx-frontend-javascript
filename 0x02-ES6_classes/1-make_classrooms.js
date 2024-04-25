5d9a2d7420c97cea69b696b77a6a7cc96147677c
Garrick Larson Potts <ricki.glp@gmail.com>
2024-04-25 15:34:22 +0000

    task 2

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from './0-classroom';
+
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+  return [room1, room2, room3];
+}
